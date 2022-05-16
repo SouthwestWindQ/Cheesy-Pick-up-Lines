b40ea7341a91bec6100e3dca5fa552caf90949ef
qianweinan <ypqwn@stu.pku.edu.cn>
2022-05-16 15:46:10 +0800

    complete the preparation of data

--- dataHelper.py
+++ dataHelper.py
@@ -3,8 +3,8 @@ import pandas as pd
 from datasets import load_dataset
 
 def get_dataset():
-    data = pd.read_excel("trial.xls")
-    pd.DataFrame.to_csv(data, "trial.csv")
-    dataset = load_dataset('csv', data_files="trial.csv")
+    data = pd.read_excel("data.xls")
+    pd.DataFrame.to_csv(data, "data.csv")
+    dataset = load_dataset('csv', data_files="data.csv")
     dataset = dataset['train'].train_test_split(train_size=0.8)
     return dataset
