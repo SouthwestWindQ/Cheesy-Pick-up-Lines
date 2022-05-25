080c2071f02136f2d88b1d9f09ad3867f267b4fa
Weinan Qian <95567950+SouthwestWindQ@users.noreply.github.com>
2022-05-25 08:28:34 +0800

    update

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 class args():
-    lr = 3e-5
-    epoch = 10
-    batch_size = 16
+    lr = 1e-4
+    epoch = 50
+    seed = 2022522
+    batch_size = 40
     max_length = 80
+    temperature = 0.8
     weight_decay = 0.01
-    output_dir = "./results"
     
-    def __init__(self):
-        pass
+    output_dir = "./results"
